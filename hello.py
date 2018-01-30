850fba7705bb13f853986ed12c1e19babec2813d
srelkins <35789181+srelkins@users.noreply.github.com>
2018-01-30 09:50:17 -0500

    added new info

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 print("My name is shannon")
 print("I was born in July")
 print ("My name is Avantika")
+
+print("my favorite food is pasta")
+print("my favorite music is alternative")
+print("I am a Leo")
